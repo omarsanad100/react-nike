e988dc9de2d6c049445dd88ba68b370582886956
Omar Sanad <omarsanad.me@gmail.com>
2024-12-30 06:36:11 +0200

    Some changes in text header

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -6,9 +6,7 @@ const PopularProducts = () => {
     <section id="products" className="max-container max-sm:pt-12">
       <div className="flex flex-col justify-start gap-5">
         <h2 className="text-4xl font-palanquin  font-bold">
-          Our
-          <span className="text-coral-red">Popular</span>
-          Products
+          Our <span className="text-coral-red">Popular</span> Products
         </h2>
         <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
           Experience top-notch quality and style with our sought-after
